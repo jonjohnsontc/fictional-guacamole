f542efa8b34f5426a394ebe1c082ae7f52a9b28a
Jon Johnson <jonjohnsontc@live.com>
2024-07-01 16:24:02 -0700

    adjusting digit based radix

--- extras/radix_2.c
+++ extras/radix_2.c
@@ -15,7 +15,7 @@ int max(int a, int b) {
 
 int main(void) {
   int input[MAX_LENGTH], output[MAX_LENGTH];
-  int count[BASE], offset[BASE];
+  int count[BASE];
   int i, n, pass, num_digits, max_num = 0;
 
   // calculate and scan input array
@@ -24,6 +24,11 @@ int main(void) {
     scanf("%d", &input[i]);
     max_num = max(input[i], max_num);
   }
+  printf("Original array:\n");
+  for (i = 0; i < n; i++) {
+    printf("%d ", input[i]);
+  }
+  printf("\n");
   num_digits = log(max_num) / log(BASE) + 1;
   memset(count, 0, 10 * sizeof(int));
   for (pass = 0; pass < num_digits; pass++) {
@@ -31,22 +36,21 @@ int main(void) {
     // add counts of each digit per BASE
     for (i = 0; i < n; i++) {
       int d = (input[i] / (int)pow(BASE, pass)) % BASE;
-      count[i] = count[d] + 1;
+      count[d]++;
     }
 
-    offset[0] = 0;
     // create cumulative # of digits per idx in arr
     for (i = 1; i < BASE; i++)
-      offset[i] = offset[i - 1] + count[i - 1];
-
+      count[i] += count[i - 1];
     // traveling through array in reverse order
     for (i = n - 1; i >= -1; i--) {
       int d = (input[i] / (int)pow(BASE, pass)) % BASE;
-      offset[d] -= 1;
-      output[offset[i]] = input[i];
+      output[count[d]] = input[i];
+      count[d] -= 1;
     }
   }
   printf("Sorted array:\n");
   for (i = 0; i < n; i++)
     printf("%d ", output[i]);
+  printf("\n");
 }
