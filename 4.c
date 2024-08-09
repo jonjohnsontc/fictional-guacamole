fdfb8f749ba13ec6e9b951bbf0b3e81434b1e5ed
Jon Johnson <jonjohnsontc@live.com>
2024-08-09 14:49:59 -0700

    just playing around with thread creation / joining

--- 4.c
+++ 4.c
@@ -13,10 +13,46 @@ the input has been read
 
 I think the threaded process is going to have some sort of queue
 */
+#include <errno.h>
+#include <pthread.h>
 #include <stdio.h>
-#include <sys/sysinfo.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define MAX_THREADS 24
+#define err_abort(code, text)                                                  \
+  do {                                                                         \
+    fprintf(stderr, "%s at \"%s\":%d:%s\n", text, __FILE__, __LINE__,          \
+            strerror(code));                                                   \
+    abort();                                                                   \
+  } while (0)
+#define errno_abort(text)                                                      \
+  do {                                                                         \
+    fprintf(stderr, "%s at \"%s\":%d:%s\n", text, __FILE__, __LINE__,          \
+            strerror(errno));                                                  \
+    abort();                                                                   \
+  } while (0)
+
+void *process(void *arg);
 int main(void) {
-  int num_procs = get_nprocs_conf();
-  printf("Number of processors configured is %d\n", num_procs);
+  pthread_t threads[MAX_THREADS];
+  int status;
+  // _SC_NPROCESSORS_ONLN is the number of currently available procs
+  int num_threads = sysconf(_SC_NPROCESSORS_CONF) / 2;
+  printf("Number of threads configured is %d\n", num_threads);
+  for (int i = 0; i < num_threads; i++) {
+    status = pthread_create(&threads[i], NULL, process, NULL);
+    if (status != 0)
+      err_abort(status, "thread creation");
+  }
+  printf("All threads created\n");
+  for (int i = 0; i < num_threads; i++)
+    status = pthread_join(threads[i], NULL);
+  printf("All threads completed\n");
   return 0;
+}
+
+void *process(void *arg) {
+  sleep(1);
+  return NULL;
 }
