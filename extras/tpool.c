561e1780bdf47468eef55a8f4beb65d865f30f4e
Jon Johnson <jonjohnsontc@live.com>
2024-08-10 11:56:50 -0700

    finished tpool example

--- extras/tpool.c
+++ extras/tpool.c
@@ -6,7 +6,9 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 struct tpool;
 typedef struct tpool tpool_t;
 
@@ -58,6 +60,7 @@ static tpool_work_t *tpool_work_get(tpool_t *tm) {
 
   if (tm == NULL)
     return NULL;
+  work = tm->work_first;
   if (work->next == NULL) {
     tm->work_first = NULL;
     tm->work_last = NULL;
@@ -94,8 +97,140 @@ static void *tpool_worker(void *arg) {
       pthread_cond_signal(&(tm->working_cond));
     pthread_mutex_unlock(&(tm->work_mutex));
   }
-  tm->thread_cnt++;
+  tm->thread_cnt--;
   pthread_cond_signal(&(tm->working_cond));
   pthread_mutex_unlock(&(tm->work_mutex));
   return NULL;
+}
+
+tpool_t *tpool_create(size_t num) {
+  tpool_t *tm;
+  pthread_t thread;
+  size_t i;
+
+  if (num == 0)
+    num = 2;
+
+  tm = calloc(1, sizeof(*tm));
+  tm->thread_cnt = num;
+
+  pthread_mutex_init(&(tm->work_mutex), NULL);
+  pthread_cond_init(&(tm->work_cond), NULL);
+  pthread_cond_init(&(tm->working_cond), NULL);
+
+  tm->work_first = NULL;
+  tm->work_last = NULL;
+
+  for (i = 0; i < num; i++) {
+    pthread_create(&thread, NULL, tpool_worker, tm);
+    pthread_detach(thread);
+  }
+  return tm;
+}
+
+void tpool_destroy(tpool_t *tm) {
+  tpool_work_t *work;
+  tpool_work_t *work2;
+
+  if (tm == NULL)
+    return;
+
+  pthread_mutex_lock(&(tm->work_mutex));
+  work = tm->work_first;
+  while (work != NULL) {
+    work2 = work->next;
+    tpool_work_destroy(work);
+    work = work2;
+  }
+
+  tm->work_first = NULL;
+  tm->stop = true;
+
+  pthread_cond_broadcast(&(tm->work_cond));
+  pthread_mutex_unlock(&(tm->work_mutex));
+
+  tpool_wait(tm);
+
+  pthread_mutex_destroy(&(tm->work_mutex));
+  pthread_cond_destroy(&(tm->work_cond));
+  pthread_cond_destroy(&(tm->working_cond));
+  free(tm);
+}
+
+bool tpool_add_work(tpool_t *tm, thread_func_t func, void *arg) {
+  tpool_work_t *work;
+
+  if (tm == NULL)
+    return false;
+
+  work = tpool_work_create(func, arg);
+  if (work == NULL)
+    return false;
+
+  pthread_mutex_lock(&(tm->work_mutex));
+  if (tm->work_first == NULL) {
+    tm->work_first = work;
+    tm->work_last = tm->work_first;
+  } else {
+    tm->work_last->next = work;
+    tm->work_last = work;
+  }
+
+  pthread_cond_broadcast(&(tm->work_cond));
+  pthread_mutex_unlock(&(tm->work_mutex));
+  return true;
+}
+
+void tpool_wait(tpool_t *tm) {
+  if (tm == NULL)
+    return;
+
+  pthread_mutex_lock(&(tm->work_mutex));
+  while (1) {
+    if (tm->work_first != NULL || (!tm->stop && tm->working_cnt != 0) ||
+        (tm->stop && tm->thread_cnt != 0)) {
+      pthread_cond_wait(&(tm->working_cond), &(tm->work_mutex));
+    } else {
+      break;
+    }
+  }
+  pthread_mutex_unlock(&(tm->work_mutex));
+}
+
+// TESTING
+static const size_t num_threads = 4;
+static const size_t num_items = 100;
+
+void worker(void *arg) {
+  int *val = arg;
+  int old = *val;
+
+  *val += 1000;
+  printf("tid=%ld, old=%d, val=%d\n", pthread_self(), old, *val);
+  if (*val % 2)
+    usleep(100000);
+}
+
+int main(int argc, char **argv) {
+  tpool_t *tm;
+  int *vals;
+  size_t i;
+
+  tm = tpool_create(num_threads);
+  vals = calloc(num_items, sizeof(*vals));
+
+  for (i = 0; i < num_items; i++) {
+    vals[i] = i;
+    tpool_add_work(tm, worker, vals + i);
+  }
+
+  tpool_wait(tm);
+
+  for (i = 0; i < num_items; i++) {
+    printf("%d\n", vals[i]);
+  }
+
+  free(vals);
+  tpool_destroy(tm);
+  return 0;
 }
