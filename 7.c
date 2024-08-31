9dd03f3fa3b6c66fd239d4a25f1beb65bbaeec7f
Jon Johnson <jonjohnsontc@live.com>
2024-08-30 17:07:36 -0700

    checkpoint

--- 7.c
+++ 7.c
@@ -1,13 +1,7 @@
 /*
-	Want to add vector intrinsicts to my previous solution
 
-	Current idea is to go through the mmapped data and find n number of rows.
-	Those n number of rows can be operated on via vector operations for each
-	metric category
+  Trying to add SIMD to my previous implementation
 
-	The one issue here is that I could potentially run into the same entry twice
-	and then I'd be doing two operations on the same piece of data, which I'm
-	assuming would land me with some undefined behavior (or just a crash)
 */
 #include <errno.h>
 #include <fcntl.h>
@@ -26,7 +20,7 @@
 #define MAX_ENTRIES 100000
 #define HASHMAP_CAPACITY 16384
 #define HASHMAP_INDEX(h) (h & (HASHMAP_CAPACITY - 1))
-#define MEASUREMENTS_FILE "./measurements_100m.txt"
+#define MEASUREMENTS_FILE "./measurements_1b.txt"
 #define err_abort(code, text)                                                  \
   do {                                                                         \
     fprintf(stderr, "%s at \"%s\":%d:%s\n", text, __FILE__, __LINE__,          \
@@ -41,7 +35,7 @@
   } while (0)
 #ifndef NTHREADS
 #define NTHREADS 12
-#endif 
+#endif
 
 static size_t chunk_count;
 static size_t chunk_size;
@@ -59,3 +53,200 @@ typedef struct group {
   Node rows[MAX_ENTRIES];
 } Group;
 
+static inline float min(float a, float b);
+static inline float max(float a, float b);
+static inline int cmp(const void *ptr_a, const void *ptr_b);
+static inline unsigned int *get_key(Group *row_grouping, char *name);
+static inline void *process_rows(void *data);
+static inline void print_data(char *buffer, Group *row_groupbing);
+
+int main(void) {
+  int fd;
+  char *addr;
+  long size;
+  fd = open(MEASUREMENTS_FILE, O_RDONLY);
+  if (fd == -1)
+    errno_abort("file open");
+
+  size = lseek(fd, 0, SEEK_END);
+  if (size == -1)
+    errno_abort("lseek");
+
+  addr = mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
+  if (*addr == -1)
+    errno_abort("mmap");
+
+  // Calculate results
+  pthread_t workers[NTHREADS];
+  chunk_size = (size / (NTHREADS * 2));
+  chunk_count = (size / chunk_size - 1) + 1;
+  for (size_t i = 0; i < NTHREADS; i++) {
+    pthread_create(&workers[i], NULL, process_rows, addr);
+  }
+
+  // wait for all threads to finish
+  Group *results[NTHREADS];
+  for (size_t i = 0; i < NTHREADS; i++) {
+    int res = pthread_join(workers[i], (void *)&results[i]);
+    if (res != 0)
+      err_abort(res, "pthread_join");
+  }
+
+  munmap((void *)addr, size);
+
+  // merge results
+  Group *combined = results[0];
+  for (size_t i = 0; i < NTHREADS; i++) {
+    for (unsigned int j = 0; j < results[i]->size; j++) {
+      Node *b = &results[i]->rows[j];
+      unsigned int *hm_entry = get_key(combined, b->name);
+      unsigned int c = *hm_entry;
+      if (c == 0) {
+        c = combined->size++;
+        *hm_entry = c;
+        strcpy(combined->rows[c].name, b->name);
+      }
+      combined->rows[c].count += b->count;
+      combined->rows[c].sum += b->sum;
+      combined->rows[c].min = min(combined->rows[c].min, b->min);
+      combined->rows[c].max = max(combined->rows[c].max, b->max);
+    }
+  }
+
+  qsort(combined->rows, combined->size, sizeof(*combined->rows), cmp);
+  char buffer[16384];
+  print_data(buffer, combined);
+  return 0;
+}
+
+static inline void *process_rows(void *_data) {
+  char *data = (char *)_data;
+
+  Group *results = malloc(sizeof(*results));
+  if (results == NULL)
+    err_abort(ENOMEM, "malloc");
+
+  results->size = 0;
+  memset(results->map, 0, HASHMAP_CAPACITY * sizeof(*results->map));
+  memset(results->rows, 0, MAX_ENTRIES * sizeof(*results->rows));
+
+  // process data until done
+  while (1) {
+    const unsigned int chunk = chunk_selector++;
+    if (chunk >= chunk_count)
+      break;
+
+    size_t chunk_start = chunk * chunk_size;
+    size_t chunk_end = chunk_start + chunk_size;
+
+    const char *start =
+        chunk_start > 0
+            ? (char *)memchr(&data[chunk_start], '\n', chunk_size) + 1
+            : &data[chunk_start];
+
+    // find pointer to last newline char, assumes file ends in a newline
+    const char *end = (char *)memchr(&data[chunk_end], '\n', chunk_size) + 1;
+
+    // 
+    while (start < end) {
+      const char *line_start = start;
+
+      // let's hash the city name until we find the delimeter ';'
+      unsigned int len = 0;
+      unsigned int hash = 5381;
+      while (*start != ';') {
+        hash = ((hash << 5) + hash) + *start++;
+        len++;
+      }
+
+      // advance past ';'
+      start++;
+
+      // parse the temperature value
+      unsigned int temp_len = 0;
+      char temp_container[10];
+      while (*start != '\n') {
+        temp_container[temp_len++] = *start++;
+      }
+      float d = strtod(temp_container, NULL);
+      if (d == 0 && errno != 0)
+        errno_abort("strtod");
+
+      // skip past newline '\n'
+      start++;
+
+      // try and find name in hashmap
+      unsigned int *loc = &results->map[HASHMAP_INDEX(hash)];
+      while (*loc > 0 &&
+             memcmp(results->rows[*loc].name, line_start, len) != 0) {
+        hash += 1;
+        loc = &results->map[HASHMAP_INDEX(hash)];
+      }
+
+      // if unseen (0) we make a new entry
+      if (*loc == 0) {
+        *loc = results->size;
+        memcpy(results->rows[*loc].name, line_start, len);
+        results->size++;
+      }
+
+      // existing entries
+      results->rows[*loc].count += 1;
+      results->rows[*loc].min = min(results->rows[*loc].min, d);
+      results->rows[*loc].max = max(results->rows[*loc].max, d);
+      results->rows[*loc].sum += d;
+    }
+  }
+  return (void *)results;
+}
+
+static inline unsigned int *get_key(Group *row_grouping, char *name) {
+  unsigned int hash = 5381;
+  unsigned int len = 0;
+  char *cur = name;
+  while (*cur != '\0') {
+    hash = ((hash << 5) + hash) + *cur++;
+    len++;
+  }
+
+  unsigned int *c = &row_grouping->map[HASHMAP_INDEX(hash)];
+  while (*c > 0 && memcmp(row_grouping->rows[*c].name, name, len) != 0) {
+    hash++;
+    c = &row_grouping->map[HASHMAP_INDEX(hash)];
+  }
+  return c;
+}
+
+static inline float max(float a, float b) {
+  if (a > b)
+    return a;
+  else
+    return b;
+}
+
+static inline float min(float a, float b) {
+  if (a < b)
+    return a;
+  else
+    return b;
+}
+
+static inline int cmp(const void *ptr_a, const void *ptr_b) {
+  return strcmp(((Node *)ptr_a)->name, ((Node *)ptr_b)->name);
+}
+
+static inline void print_data(char *buffer, Group *row_grouping) {
+  *buffer++ = '{';
+  for (unsigned int i = 0; i < row_grouping->size; i++) {
+    size_t n = (size_t)sprintf(
+        buffer, "%s=%.1f/%.1f/%.1f%s", row_grouping->rows[i].name,
+        row_grouping->rows[i].min,
+        row_grouping->rows[i].sum / row_grouping->rows[i].count,
+        row_grouping->rows[i].max, i < (row_grouping->size - 1) ? ", " : "");
+
+    buffer += n;
+  }
+  *buffer++ = '}';
+  *buffer++ = '\n';
+  *buffer++ = '\0';
+}
--- extras/simd.c
+++ extras/simd.c
@@ -1,81 +1,80 @@
 #include "simd.h"
 #include <assert.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
-		Trying to get an understanding of which SIMD / instrinsic
-		functions are available for my system, and  how to best use
-		them.
+    Trying to get an understanding of which SIMD / instrinsic
+    functions are available for my system, and  how to best use
+    them.
 */
-float* make_float_arr(void);
+float *make_float_arr(void);
 int main(void) {
-	float arr_1[] = { 2, 3, 7, 72 };
-	float arr_2[] = { 8, 7, 2, 12 };
+  float arr_1[] = {2, 3, 7, 72};
+  float arr_2[] = {8, 7, 2, 12};
 
-	__m128 loaded1 = _mm_loadu_ps((const float*)&arr_1);
-	__m128 loaded2 = _mm_loadu_ps((const float*)&arr_2);
-	__m128 result = _mm_add_ps(loaded1, loaded2);
+  __m128 loaded1 = _mm_loadu_ps((const float *)&arr_1);
+  __m128 loaded2 = _mm_loadu_ps((const float *)&arr_2);
+  __m128 result = _mm_add_ps(loaded1, loaded2);
 
-	float arr_3[4];
-	_mm_store_ps((float*)&arr_3, result);
+  float arr_3[4];
+  _mm_store_ps((float *)&arr_3, result);
 
-	assert(arr_3[0] == 10.0);
-	assert(arr_3[1] == 10.0);
-	assert(arr_3[2] == 9.0);
-	assert(arr_3[3] == 84.0);
+  assert(arr_3[0] == 10.0);
+  assert(arr_3[1] == 10.0);
+  assert(arr_3[2] == 9.0);
+  assert(arr_3[3] == 84.0);
 
-	float* arr_4 = make_float_arr();
-	x4_add(&arr_4, arr_1, arr_2);
-	assert(arr_4[0] == 10.0);
-	assert(arr_4[1] == 10.0);
-	assert(arr_4[2] == 9.0);
-	assert(arr_4[3] == 84.0);
+  float *arr_4 = make_float_arr();
+  x4_add(&arr_4, arr_1, arr_2);
+  assert(arr_4[0] == 10.0);
+  assert(arr_4[1] == 10.0);
+  assert(arr_4[2] == 9.0);
+  assert(arr_4[3] == 84.0);
 
-	float* arr_5 = make_float_arr();
-	arr_5[0] = 11.0;
-	arr_5[1] = 32.0;
-	x4_pad(&arr_5, 2);
-	assert(arr_5[0] == 11.0);
-	assert(arr_5[1] == 32.0);
-	assert(arr_5[2] == 0.0);
-	assert(arr_5[3] == 0.0);
+  float *arr_5 = make_float_arr();
+  arr_5[0] = 11.0;
+  arr_5[1] = 32.0;
+  x4_pad(&arr_5, 2);
+  assert(arr_5[0] == 11.0);
+  assert(arr_5[1] == 32.0);
+  assert(arr_5[2] == 0.0);
+  assert(arr_5[3] == 0.0);
 
-	float* arr_6 = make_float_arr();
-	x4_pad(&arr_6, 4);
-	assert(arr_6[0] == 0.0);
-	assert(arr_6[1] == 0.0);
-	assert(arr_6[2] == 0.0);
-	assert(arr_6[3] == 0.0);
+  float *arr_6 = make_float_arr();
+  x4_pad(&arr_6, 4);
+  assert(arr_6[0] == 0.0);
+  assert(arr_6[1] == 0.0);
+  assert(arr_6[2] == 0.0);
+  assert(arr_6[3] == 0.0);
 
-	free(arr_4);
-	free(arr_5);
-	free(arr_6);
-	return 0;
+  free(arr_4);
+  free(arr_5);
+  free(arr_6);
+  return 0;
 }
 
-float* make_float_arr(void) {
-	float* arr = malloc(sizeof(float) * 4);
-	if (arr == NULL) {
-		fprintf(stderr, "malloc err\n");
-		exit(1);
-	}
-	return arr;
+float *make_float_arr(void) {
+  float *arr = malloc(sizeof(float) * 4);
+  if (arr == NULL) {
+    fprintf(stderr, "malloc err\n");
+    exit(1);
+  }
+  return arr;
 }
 
-void x4_add(float** dest, float* arr_1, float* arr_2) {
-	__m128 loaded1 = _mm_loadu_ps((const float*)arr_1);
-	__m128 loaded2 = _mm_loadu_ps((const float*)arr_2);
-	__m128 result = _mm_add_ps(loaded1, loaded2);
-	_mm_store_ps(*dest, result);
+void x4_add(float **dest, float *arr_1, float *arr_2) {
+  __m128 loaded1 = _mm_loadu_ps((const float *)arr_1);
+  __m128 loaded2 = _mm_loadu_ps((const float *)arr_2);
+  __m128 result = _mm_add_ps(loaded1, loaded2);
+  _mm_store_ps(*dest, result);
 }
 
-void x4_pad(float** arr, unsigned int n) {
-	if (n > 4) {
-		fprintf(stderr, "Invalid amount passed\n");
-		exit(1);
-	}
-	memset(*arr + (4 - n), 0, n * sizeof(float));
+void x4_pad(float **arr, unsigned int n) {
+  if (n > 4) {
+    fprintf(stderr, "Invalid amount passed\n");
+    exit(1);
+  }
+  memset(*arr + (4 - n), 0, n * sizeof(float));
 }
-
