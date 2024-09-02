402d723a982b34cb66321cf3117fa2fa7ebdafa2
Jon Johnson <jonjohnsontc@live.com>
2024-09-02 12:30:15 -0700

    checkpoint

--- extras/check_locale.c
+++ extras/check_locale.c
@@ -10,14 +10,17 @@ to handle UTF-8 character sorting, but running it against a few different exampl
 hasn't delivered the results I would've expected. It also appears to be 
 non-deterministic on my WSL Debian machine
 
+EDIT: Turns out that I wasn't properly assigning the correct pointer values as args
+to both comparison functions. Now that I am, things seem to work correctly.
+
 */
 
 static inline int collate(const void *ptr_a, const void *ptr_b) {
-  return strcoll((char *)ptr_b, (char *)ptr_a);
+  return strcoll(*(char **)ptr_a, *(char **)ptr_b);
 }
 
 static inline int cmp(const void *ptr_a, const void *ptr_b) {
-  return strcmp((char *)ptr_b, (char *)ptr_a);
+  return strcmp(*(char **)ptr_a, *(char **)ptr_b);
 }
 
 int main(void) {
@@ -29,15 +32,28 @@ int main(void) {
 
   char *words[6] = {"Zürich",  "Ürümqi",   "Wrocław",
                     "Oklahoma", "Pittsburgh", "Bouaké"};
+  char *letters[6] = {"Z", "Ü", "W", "O", "P", "B"};
   qsort(words, 6, sizeof(char *), cmp);
-  printf("strcmp order: ");
+  qsort(letters, 6, sizeof(char *), cmp);
+  printf("strcmp order: \n");
+  printf("words: ");
   for (int i = 0; i < 6; i++)
     printf("%s ", words[i]);
   printf("\n");
-
+  printf("letters: ");
+  for (int i = 0; i < 6; i++)
+    printf("%s ", letters[i]);
+  printf("\n");
+  
   qsort(words, 6, sizeof(char *), collate);
+  qsort(letters, 6, sizeof(char *), collate);
   printf("strcoll order: ");
   for (int i = 0; i < 6; i++)
     printf("%s ", words[i]);
   printf("\n");
+  printf("letters: ");
+  for (int i = 0; i < 6; i++)
+    printf("%s ", letters[i]);
+  printf("\n");
+
 }
